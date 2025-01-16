dd32eaa4c95aed94fdf509fdeb33343733da2f0f
ermanjunaidi <ermanjunaidi1@gmail.com>
2025-01-16 14:45:13 +0700

    02

--- index.js
+++ index.js
@@ -43,57 +43,95 @@
 // console.log(string.charAt(15)) // S
 
 // 14.What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+// let string = '30 Days Of JavaScript'
+// console.log(string.charCodeAt('J'))
+
 // 15.Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+// let string = '30 Days Of JavaScript'
+// console.log(string.indexOf('3'))
+
 // 16.Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+// let string = '30 Days Of JavaScript'
+// console.log(string.lastIndexOf('a'))
+
 // 17.Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// let string = "You cannot end a sentence with because because because is a conjunction"
+// console.log(string.indexOf('because'))
+
 // 18.Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// let string = "You cannot end a sentence with because because because is a conjunction"
+// console.log(string.lastIndexOf('because'))
+
 // 19.Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// let string = "You cannot end a sentence with because because because is a conjunction"
+// console.log(string.search('because'))
+
 // 20.Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+// let string = '  30 Days Of JavaScript'
+// console.log(string.trim(' '))
+
 // 21.Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+// let string = '30 Days Of JavaScript'
+// console.log(string.startsWith('30'))
+
 // 22.Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+// let string = '30 Days Of JavaScript'
+// console.log(string.endsWith('JavaScript'))
+
 // 23.Use match() method to find all the a’s in 30 Days Of JavaScript
+// let string = '30 Days Of JavaScript'
+// console.log(string.match(/30/g))
+
 // 24.Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+// let string1 = "30 Days of"
+// let string2 = "JavaScript"
+// console.log(`${string1} ${string2}`)
+
 // 25.Use repeat() method to print 30 Days Of JavaScript 2 times
+let string = "30 Days Of JavaScript"
+console.log((string + '\n') .repeat(30).trim())
 
 // Exercise: Level 2
 
-// Using console.log() print out the following statement:
-
+// 01.Using console.log() print out the following statement:
 // The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
-// Using console.log() print out the following quote by Mother Teresa:
+// console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
 
+// 02.Using console.log() print out the following quote by Mother Teresa:
 // "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
-// Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
 
-// Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+// 03.Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
 
-// Check if 'on' is found in both python and jargon
+// 04.Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
 
-// I hope this course is not full of jargon. Check if jargon is in the sentence.
+// 05.Check if 'on' is found in both python and jargon
 
-// Generate a random number between 0 and 100 inclusively.
+// 06.I hope this course is not full of jargon. Check if jargon is in the sentence.
 
-// Generate a random number between 50 and 100 inclusively.
+// 07.Generate a random number between 0 and 100 inclusively.
 
-// Generate a random number between 0 and 255 inclusively.
+// 08.Generate a random number between 50 and 100 inclusively.
 
-// Access the 'JavaScript' string characters using a random number.
+// 09.Generate a random number between 0 and 255 inclusively.
 
-// Use console.log() and escape characters to print the following pattern.
+// 10.Access the 'JavaScript' string characters using a random number.
+
+// 11.Use console.log() and escape characters to print the following pattern.
 
 // 1 1 1 1 1
 // 2 1 2 4 8
 // 3 1 3 9 27
 // 4 1 4 16 64
 // 5 1 5 25 125
-// Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+// 12.Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
 // Exercises: Level 3
-// 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+// 01.'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
 
-// Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// 02.Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
-// Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+// 03.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+// const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
-//     const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-// Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+//04.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
