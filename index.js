deb80c95bd1af677cd3793dfc550db31df88352e
ermanjunaidi <ermanjunaidi1@gmail.com>
2025-01-20 07:27:30 +0700

    03

--- index.js
+++ index.js
@@ -88,8 +88,8 @@
 // console.log(`${string1} ${string2}`)
 
 // 25.Use repeat() method to print 30 Days Of JavaScript 2 times
-let string = "30 Days Of JavaScript"
-console.log((string + '\n') .repeat(30).trim())
+// let string = "30 Days Of JavaScript"
+// console.log((string + '\n') .repeat(30).trim())
 
 // Exercise: Level 2
 
@@ -99,39 +99,58 @@ console.log((string + '\n') .repeat(30).trim())
 
 // 02.Using console.log() print out the following quote by Mother Teresa:
 // "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+// console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
 
 // 03.Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+// let value = '10';
+// if (typeof value !== 'number') {
+//     value = Number(value); // Convert the string to a number
+// }
+// console.log(value === 10); 
 
 // 04.Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
-
+// let value='9.8';
+// if (typeof value !== 'number') {
+//     value = Number(value); // Convert the string to a number
+// }
+// console.log(value === 10);
 // 05.Check if 'on' is found in both python and jargon
+// let word1 = "python";
+// let word2 = "jargon";
+// let result = word1.includes("on") && word2.includes("on");
+// console.log(result); // true
 
 // 06.I hope this course is not full of jargon. Check if jargon is in the sentence.
-
-// 07.Generate a random number between 0 and 100 inclusively.
+// let sentence = "I hope this course is not full of jargon";
 
 // 08.Generate a random number between 50 and 100 inclusively.
+// console.log(Math.floor(Math.random() * 51) + 50);
 
 // 09.Generate a random number between 0 and 255 inclusively.
-
-// 10.Access the 'JavaScript' string characters using a random number.
+// let string = "JavaScript";
+// console.log(Math.floor(Math.random() * string.length));
 
 // 11.Use console.log() and escape characters to print the following pattern.
-
-// 1 1 1 1 1
-// 2 1 2 4 8
-// 3 1 3 9 27
-// 4 1 4 16 64
-// 5 1 5 25 125
+//  1 1 1 1 1
+//  2 1 2 4 8
+//  3 1 3 9 27
+//  4 1 4 16 64
+//  5 1 5 25 125
+// console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125')
 
 // 12.Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// console.log("You cannot end a sentence with because because because is a conjunction".substr(31, 20))
 
 // Exercises: Level 3
 // 01.'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+// console.log("Love is the best thing in this world. Some found their love and some are still looking for their love.".match(/love/g).length)
 
 // 02.Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// console.log("You cannot end a sentence with because because because is a conjunction".match(/because/g).length)
 
 // 03.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
 // const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+// console.log(sentence.replace(/[^\w\s]/g, '').replace(/\s+/g, ' ').split(' ').sort((a, b) => b.length - a.length)[0])
 
 //04.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+// console.log((5000 + 10000 + 15000) * 12)
